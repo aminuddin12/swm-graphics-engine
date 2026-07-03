888257639d3c2007939d9b1f587f31a1b9a1edbe
aminuddin12 <aminuddinadl12@icloud.com>
2026-07-03 18:50:32 +0700

    fix: Resolve clangd parser confusion in FrameStatistics.h

--- include/swm/diagnostics/FrameStatistics.h
+++ include/swm/diagnostics/FrameStatistics.h
@@ -4,9 +4,9 @@
 namespace swm::graphics {
 
 struct FrameStatistics {
-    float frameTimeMilliseconds{0.0f};
-    float cpuRenderTimeMilliseconds{0.0f};
-    float gpuRenderTimeMilliseconds{0.0f};
+    float frameTimeMilliseconds = 0.0f;
+    float cpuRenderTimeMilliseconds = 0.0f;
+    float gpuRenderTimeMilliseconds = 0.0f;
 };
 
 }
